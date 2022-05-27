302f89ab9a5d8c0e8a43cd3a61d2884f23014e9f
José Luis Velázquez Rodríguez <joseluisvelazquezro@gmail.com>
2022-05-27 15:58:58 -0500

    strStr2

--- strStr.py
+++ strStr.py
@@ -1,9 +1,22 @@
 def strStr(haystack, needle):
+    if len(needle) > len(haystack):
+        return -1
     i = 0
-    while i < len(haystack):
+    while i < len(haystack) - len(needle) + 1:
+        if haystack[i] == needle[0]:
+            j = 1
+            flag = 0
+            while j < len(needle):
+                if haystack[i+j] != needle[j]:
+                    flag = 1
+                    break
+                j += 1
+            if flag == 0:
+                return i
+                
         i += 1
-    return 0
+    return -1
     
-haystack = "hello"
-needle = "ll"
+haystack = "aaaaa"
+needle = "bba"
 print(strStr(haystack,needle))
